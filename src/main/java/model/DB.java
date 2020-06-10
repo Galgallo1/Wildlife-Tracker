e5fc3a9e1c6b2946f03e73ad856bc06a5d308a3c
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-06-10 17:36:46 +0300

    add more maven dependencies

--- src/main/java/model/DB.java
+++ src/main/java/model/DB.java
@@ -4,6 +4,6 @@ import org.sql2o.Sql2o;
 
 public class DB {
     //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "gaalo", "1234");
-    String connectionString = "jdbc:postgresql://ec2-3-231-16-122.compute-1.amazonaws.com:5432/dfkv24r3dm8do0"; //!
-    Sql2o sql2o = new Sql2o(connectionString, "dgcktkncwavuqf", "5426a79a23c52e020bd58eb0173b8dba44d741062270fde0cca4a6ce2935d20e"); //!
+    public static String connectionString = "jdbc:postgresql://ec2-3-231-16-122.compute-1.amazonaws.com:5432/dfkv24r3dm8do0"; //!
+    public static Sql2o sql2o = new Sql2o(connectionString, "dgcktkncwavuqf", "5426a79a23c52e020bd58eb0173b8dba44d741062270fde0cca4a6ce2935d20e"); //!
 }
